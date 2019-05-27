1a70f413b9387b5bed9159944a21ab68e68274e9
github username <github email address>
2019-05-27 00:47:26 -0400

    added recipe form

---
+++ src/Page.js
@@ -0,0 +1,79 @@
+class Page {
+    constructor(user) {
+        this.user = user
+        Page.loggedIn.user = this.user
+    }
+
+    static renderLogin() {
+        const root = document.getElementById('root')
+        root.innerHTML = ''
+
+
+        const formWrapper = document.createElement('div')
+        formWrapper.classList.add('login-wrapper')
+        root.appendChild(formWrapper)
+
+        const siteTitle = document.createElement('h1')
+        siteTitle.classList.add('site-title')
+        siteTitle.innerText = "Chef You"
+        formWrapper.appendChild(siteTitle)
+
+        const loginForm = document.createElement('form')
+        loginForm.classList.add('form', 'login')
+        loginForm.addEventListener('submit', User.login)
+        formWrapper.appendChild(loginForm)
+
+        const usernameInput = document.createElement('input')
+        usernameInput.placeholder = 'Please Enter Your Username'
+        loginForm.appendChild(usernameInput)
+
+        const submit = document.createElement('input')
+        submit.type = 'submit'
+        submit.value = 'Login'
+        loginForm.appendChild(submit)
+
+        const registerLink = document.createElement("a")
+        registerLink.innerText = 'Register'
+        registerLink.dataset.isRegister = 0
+        registerLink.addEventListener("click", Page.toggleRegister)
+        formWrapper.appendChild(registerLink)
+
+    }
+
+    static toggleRegister(e) {
+        let isRegister = parseInt(e.target.dataset.isRegister)
+        const loginForm = document.querySelector('form.login')
+
+        if(!isRegister) {
+            loginForm.removeEventListener('submit', User.login)
+            loginForm.children[1].value = 'Register'
+            loginForm.addEventListener('submit', User.register)
+
+            isRegister ++
+            e.target.dataset.isRegister = isRegister
+            e.target.innerText = 'Login'
+        } else {
+            loginForm.removeEventListener('submit', User.register)
+            loginForm.children[1].value = 'Login'
+            loginForm.addEventListener('submit', User.login)
+
+            isRegister --
+            e.target.dataset.isRegister = isRegister
+            e.target.innerText = 'Register'
+        }
+    }
+
+    static configObj(method, body)  {
+        return {
+            method: method,
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": 'application/json'
+            },
+            body: JSON.stringify(body)
+        }
+    }
+
+
+
+}
---
+++ src/User.js
@@ -0,0 +1,168 @@
+class User {
+
+    constructor(params) {
+        this.id = params.id
+        this.username = params.username
+        this.recipes = params.recipes.map(recipe => new Recipe(recipe))
+    }
+
+    static register(e) {
+        e.preventDefault()
+
+        const body = {
+            username: e.target.firstElementChild.value
+        }
+
+        fetch(USERS_URL, Page.configObj('POST', body))
+            .then(resp => resp.json())
+            .then(user => {
+                const loggedIn = new User(user)
+                loggedIn.render()
+            })
+    }
+
+    static login(e) {
+        e.preventDefault()
+        const username = e.target.firstElementChild.value
+
+        fetch(USERS_URL)
+            .then(resp => resp.json())
+            .then(users => {
+                const thisUser = users.find(user => user.username === username)
+                const loggedIn = new User(thisUser)
+                loggedIn.render()
+            })
+            .catch(console.log)
+    }
+
+    render() {
+        const root = document.getElementById('root')
+        root.innerHTML = ''
+        root.classList.add('open')
+
+        const title = document.createElement('h1')
+        title.classList.add('page-title')
+        title.innerText = 'The Greatest Recipes in the World'
+        root.appendChild(title)
+
+        const author = document.createElement('h3')
+        author.innerText = `By ${this.username}`
+        root.appendChild(author)
+
+        const contentsWrapper = document.createElement('div')
+        root.appendChild(contentsWrapper)
+
+        const recipeSubheader = document.createElement("h2")
+        recipeSubheader.innerText = 'Recipes'
+        contentsWrapper.appendChild(recipeSubheader)
+
+        const addRecipe = document.createElement('a')
+        addRecipe.innerText = "+ add a recipe"
+        addRecipe.addEventListener('click', e => this.renderRecipeForm(e))
+        contentsWrapper.appendChild(addRecipe)
+
+        const recipes = document.createElement('ul')
+        recipes.id = 'recipe-list'
+        contentsWrapper.appendChild(recipes)
+
+
+        this.recipes.forEach(recipe => recipe.renderIndex(recipes))
+
+    }
+
+    renderRecipeForm(e) {
+        const root = document.getElementById('root')
+
+        const overlay = document.createElement('div')
+        overlay.classList.add('overlay')
+        root.appendChild(overlay)
+
+        const formWrapper = document.createElement('div')
+        formWrapper.classList.add('form-wrapper')
+        root.appendChild(formWrapper)
+
+        const pageTitle = document.createElement('h1')
+        pageTitle.classList.add('page-title')
+        pageTitle.innerText = 'Add A New Recipe'
+        formWrapper.appendChild(pageTitle)
+
+        const form = document.createElement('form')
+        form.classList.add('recipe-form')
+        form.addEventListener('submit', e => this.createRecipe(e))
+        formWrapper.appendChild(form)
+
+        const name = document.createElement('input')
+        name.placeholder = 'Enter Your Recipe Name'
+        form.appendChild(name)
+
+        const servingsWrapper = document.createElement('div')
+        form.appendChild(servingsWrapper)
+
+        const servings = document.createElement('input')
+        servings.type = 'number'
+        servings.placeholder = 'Enter number of servings'
+        servingsWrapper.appendChild(servings)
+
+        const description = document.createElement('textarea')
+        description.placeholder = "Enter a description of this recipe"
+        form.appendChild(description)
+
+        const ingredients = document.createElement('ul')
+        ingredients.classList.add('form-ingredients')
+        form.appendChild(ingredients)
+        User.addIngredient(e)
+
+        const directions = document.createElement('ol')
+        directions.classList.add('form-directions')
+        form.appendChild(directions)
+        User.addDirection(e)
+
+        const submit = document.createElement('input')
+        submit.type = 'submit'
+        submit.value = 'Add This Recipe'
+        form.appendChild(submit)
+    }
+
+    static addIngredient(e) {
+        e.target.removeEventListener('input', User.addIngredient)
+        const ingredients = document.querySelector('.form-ingredients')
+
+        const ingredientRow = document.createElement('li')
+        ingredients.appendChild(ingredientRow)
+
+        const qty = document.createElement('input')
+        qty.type = 'number'
+        ingredientRow.appendChild(qty)
+
+        const unit = document.createElement('input')
+        ingredientRow.appendChild(unit)
+
+        const ingredient = document.createElement('input')
+        ingredient.addEventListener('input', User.addIngredient)
+        ingredientRow.appendChild(ingredient)
+    }
+
+    static addDirection(e) {
+        e.target.removeEventListener('input', User.addDirection)
+
+        const directions = document.querySelector('.form-directions')
+
+        const directionWrapper = document.createElement('li')
+        directions.appendChild(directionWrapper)
+
+        const direction = document.createElement('textarea')
+        direction.addEventListener('input', User.addDirection)
+        directionWrapper.appendChild(direction)
+    }
+
+    createRecipe(e) {
+        e.preventDefault()
+
+        const body = {
+            user_id: this.id,
+            name: e.target[0].value
+        }
+
+    }
+
+}
