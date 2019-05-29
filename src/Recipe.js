a406572d5cc128c4d44915f90bdedfc0e5945fd1
github username <github email address>
2019-05-29 10:41:29 -0400

    added edit icon

--- src/Recipe.js
+++ src/Recipe.js
@@ -37,10 +37,15 @@ class Recipe {
         pageOne.classList.add('page-one')
         root.appendChild(pageOne)
 
+
         const title = document.createElement('h1')
         title.classList.add('page-title')
         title.innerText = this.name
         pageOne.appendChild(title)
+
+        const editLink = document.createElement('div')
+        editLink.classList.add('far', 'fa-edit', 'float-right')
+        pageOne.appendChild(editLink)
 
         const description = document.createElement('p')
         description.innerText = this.description
@@ -82,6 +87,56 @@ class Recipe {
     }
 
     renderEdit(e) {
+        const root = document.getElementById('root')
+
+        const overlay = document.createElement('div')
+        overlay.classList.add('overlay')
+        root.appendChild(overlay)
+
+        const formWrapper = document.createElement('div')
+        formWrapper.classList.add('form-wrapper')
+        root.appendChild(formWrapper)
+
+        const pageTitle = document.createElement('h1')
+        pageTitle.classList.add('page-title')
+        pageTitle.innerText = 'Add A New Recipe'
+        formWrapper.appendChild(pageTitle)
+
+        const form = document.createElement('form')
+        form.classList.add('recipe-form')
+        form.addEventListener('submit', e => this.createRecipe(e))
+        formWrapper.appendChild(form)
+
+        const name = document.createElement('input')
+        name.placeholder = 'Enter Your Recipe Name'
+        form.appendChild(name)
+
+        const servingsWrapper = document.createElement('div')
+        form.appendChild(servingsWrapper)
+
+        const servings = document.createElement('input')
+        servings.type = 'number'
+        servings.placeholder = 'Enter number of servings'
+        servingsWrapper.appendChild(servings)
+
+        const description = document.createElement('textarea')
+        description.placeholder = "Enter a description of this recipe"
+        form.appendChild(description)
+
+        const ingredients = document.createElement('ul')
+        ingredients.classList.add('form-ingredients')
+        form.appendChild(ingredients)
+        User.addIngredientField(e)
+
+        const directions = document.createElement('ol')
+        directions.classList.add('form-directions')
+        form.appendChild(directions)
+        User.addDirectionField(e)
+
+        const submit = document.createElement('input')
+        submit.type = 'submit'
+        submit.value = 'Add This Recipe'
+        form.appendChild(submit)
 
     }
 }
